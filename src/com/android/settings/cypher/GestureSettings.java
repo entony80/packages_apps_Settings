de966a095d5f7f932c9ee42c74d8d6492d943dfd
Sirhc95 <chriscrawf95@gmail.com>
2016-04-10 19:36:58 -0700

    Fixed: Gesture Settings Class

--- src/com/android/settings/cypher/GestureSettings.java
+++ src/com/android/settings/cypher/GestureSettings.java
@@ -16,62 +16,41 @@
 
 package com.android.settings.cypher;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.preference.PreferenceCategory;
+import android.content.res.Resources;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
-import android.provider.SearchIndexableResource;
 
-import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settings.search.Indexable;
+import com.android.settings.R;
+import android.provider.settings;
 import com.android.settings.Utils;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.cyanogenmod.internal.logging.CMMetricsLogger;
+import com.android.internal.logging.MetricsLogger;
 
 public class GestureSettings extends SettingsPreferenceFragment
-        implements Indexable {
+        implements Preference.OnPreferenceChangeListener {
 
-    private static final String TAG = GestureSettings.class.getSimpleName();
+    private static final String TAG = "GestureSettings";
+	
+	 @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.APPLICATION;
+    }
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-
         addPreferencesFromResource(R.xml.gesture_settings);
-        final PreferenceScreen prefScreen = getPreferenceScreen();
-
     }
 
     @Override
-    protected int getMetricsCategory() {
-        // todo add a constant in MetricsLogger.java
-        return CMMetricsLogger.MAIN_SETTINGS;
+    public void onResume() {
+        super.onResume();
     }
 
-    public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider() {
-                @Override
-                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
-                                                                            boolean enabled) {
-                    ArrayList<SearchIndexableResource> result =
-                            new ArrayList<SearchIndexableResource>();
-
-                    SearchIndexableResource sir = new SearchIndexableResource(context);
-                    sir.xmlResId = R.xml.gesture_settings;
-                    result.add(sir);
-
-                    return result;
-                }
-
-                @Override
-                public List<String> getNonIndexableKeys(Context context) {
-                    ArrayList<String> result = new ArrayList<String>();
-                    return result;
-                }
-            };
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        return false;
+    }
 }
